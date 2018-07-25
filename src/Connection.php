fb85111abe99693023453d8ec25a054f220dc308
Samuel Gomes <hellish.doom@gmail.com>
2018-07-25 02:58:58 -0300

    Adicionado use para PDO

---
+++ src/Connection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Src;
+
+use \PDO;
+
+class Connection
+{
+    protected $conn;
+
+    public function __construct()
+    {
+    }
+
+    public function getConnection()
+    {
+        try {
+            return $this->conn = new PDO(
+                'mysql:host=localhost;dbname=db_simple_crud;charset=utf8',
+                'root',
+                '',
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_PERSISTENT => false,
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
+                    ]
+            );
+        } catch (\PDOException $erro) {
+            return $erro->getMessage();
+        }
+    }
+}

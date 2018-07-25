ca8e43f3aa0fb21360ac11a8d39ec7db8463c38b
Samuel Gomes <hellish.doom@gmail.com>
2018-07-25 02:59:48 -0300

    Add namespace e use para PDO

---
+++ src/Professor.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Src;
+
+use \PDO;
+
+class Professor
+{
+    public static function getAllProfessor()
+    {
+        $pdo = new Connection();
+        $query = 'SELECT * FROM professor';
+        $getProf = $pdo->getConnection()->prepare($query);
+        $getProf->execute();
+        $results = $getProf->fetchAll(PDO::FETCH_ASSOC);
+        return $results;
+    }
+
+    public static function getProfessorById($id)
+    {
+        $pdo = new Connection();
+        $query = 'SELECT * FROM professor WHERE id = :id';
+        $getProf = $pdo->getConnection()->prepare($query);
+        $getProf->bindParam(':id', $id);
+        $getProf->execute();
+        $results = $getProf->fetch(PDO::FETCH_ASSOC);
+        return $results;
+    }
+
+    public static function deleteProfessor($id)
+    {
+        $pdo = new Connection();
+        $query = 'DELETE FROM professor WHERE id = :id';
+        $getProf = $pdo->getConnection()->prepare($query);
+        $getProf->bindParam(':id', $id);
+        $getProf->execute();
+        return 'Registro com ID = ' . $id . ' excluido com sucesso!';
+    }
+
+    public static function salvarProfessor($dados = [])
+    {
+        $pdo = new Connection();
+        $query = 'INSERT INTO professor (nome, telefone, endereco) VALUES(:nome, :telefone, :endereco) ';
+        $getProf = $pdo->getConnection()->prepare($query);
+        $getProf->bindParam(':nome', $dados['nome']);
+        $getProf->bindParam(':telefone', $dados['telefone']);
+        $getProf->bindParam(':endereco', $dados['endereco']);
+        $getProf->execute();
+
+        return 'Registro adicionado com sucesso!';
+    }
+
+    public static function atualizarProfessor($dados = [])
+    {
+        $pdo = new Connection();
+        $query = 'UPDATE professor SET nome = :nome, telefone = :telefone, endereco = :endereco WHERE id = :id ';
+        $getProf = $pdo->getConnection()->prepare($query);
+        $getProf->bindParam(':id', $dados['id']);
+        $getProf->bindParam(':nome', $dados['nome']);
+        $getProf->bindParam(':telefone', $dados['telefone']);
+        $getProf->bindParam(':endereco', $dados['endereco']);
+        $getProf->execute();
+
+        return 'Registro atualizado com sucesso!';
+    }
+}
